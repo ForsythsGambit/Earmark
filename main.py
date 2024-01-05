2bdd5a4cda208da047d02548f1557b16344f6a40
Liam Crowley <theadvfred@gmail.com>
2024-01-04 22:47:47 -0600

    Added func. trimFile and cmd line execution handling Using ffmpy (which wraps ffmpeg) trimFile trims the specified audio file A if name==main block was added which allows the file to be called from the terminal and execute a function followed by its arguments.

---
+++ main.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import ffmpy
+import sys
+
+def trimFile(inFile, outFile, time=15):
+	ff = ffmpy.FFmpeg( inputs={inFile : None}, outputs={ outFile: f"-t {time}"})
+	#ff.cmd.append('-loglevel')
+	#ff.cmd.append('quiet')
+	ff.run()
+
+
+if __name__ == '__main__':
+	# little bit of stackoverflow magic which should let me call functions from the command like so:
+	#python3 main.py trimFle test.mp3 ttest.mp3
+	args = sys.argv
+	# args[0] = current file
+	# args[1] = function name
+	# args[2:] = function args : (*unpacked)
+	try:
+		globals()[args[1]](*args[2:])
+	except IndexError:
+		#script was called without any commandline arguments, otherwise it would error with IndexError
+		pass
