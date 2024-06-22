657d34233a332d0c6d5325debe893e0595cec2cd
Liam Crowley <liamtavishcrowley@gmail.com>
2024-06-22 15:09:04 -0500

    Added type hints to audio.py

--- audio.py
+++ audio.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
 import speech_recognition as sr
 from thefuzz import fuzz 
-import sys
 import dotenv
 import os
 import logging
 import subprocess
 """Transcription Functions"""
 
-def prepFile(inputFile, time=15):
+def prepFile(inputFile: str, time=15) -> str:
 	"""takes inputed audio file, trims it keeping only the first {time} seconds and converts it to a .wav for transcription"""
-	outputPath=f"{os.getcwd()}/temp/{os.path.basename(inputFile).split('.')[0]}.wav"
+	outputPath: str = f"{os.getcwd()}/temp/{os.path.basename(inputFile).split('.')[0]}.wav"
 	logging.debug(f"Prepping file {inputFile}")
 	subprocess.run(["ffmpeg","-i",inputFile,"-t",str(time),outputPath,"-loglevel","error"])
 	
 	return outputPath
 
-def transcribe(inputFile):
+def transcribe(inputFile) -> str:
 	dotenv.load_dotenv() #load google cloud API key from .env to its enviroment variable
-	recogonizer=sr.Recognizer() #init speech_recognition
+	recogonizer = sr.Recognizer() #init speech_recognition
 	#convert .wav file into an AudioFile instance
 	audioFile = sr.AudioFile(inputFile)  
 	with audioFile as source:
